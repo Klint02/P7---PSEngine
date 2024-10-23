dfa78e22d36f7bb331a0ff3326d26d20e0b9d321
Rasm832b <93643276+Rasm832b@users.noreply.github.com>
2024-10-23 12:42:35 +0200

    uploaded HTML and CSS files for frontend prototype (#17)
    * uploaded HTML and CSS files for frontend prototype
    
    * frontend development
    
    har fixet nogle ting til frontenden, som position af divs og en tabel med kommandoer.
    
    * Ny struktur
    
    Har ændre strukturen så vi bruger display: grid alle steder. Her er et link til hvordan det virker: https://www.w3schools.com/css/css_grid.asp
    
    * Small styling changes
    
    * Added keyword search bar
    
    * Changed inline commands
    
    * Added more buttons
    
    * Fixed font
    
    * Removed "Recently" block
    
    * Changed from grid to flex
    
    * lidt fixes
    
    har lavet lidt fixes rund omkring
    
    * fixed searchbody being squashed
    
    * moved files into src folder
    
    * moved files into frontend folder
    
    * changed file stucture
    
    * frontend now works (for now) :)
    
    * Restored License
    
    ---------
    
    Co-authored-by: Torbjørn <74594227+TorbjoernH@users.noreply.github.com>
    Co-authored-by: TorbjoernH <thelst20@student.aau.dk>

--- src/P7-PSEngine/API/ProductEndpointsExt.cs
+++ src/P7-PSEngine/API/ProductEndpointsExt.cs
@@ -84,14 +84,14 @@ namespace P7_PSEngine.API
                 return Results.NoContent();
             });
 
-            //string static_path = "/app/wwwroot";
+            string static_path = "/app/wwwroot";
 
             //Lets you send files from wwwroot folder
             app.UseStaticFiles();
 
             //examples on how you can send data or pages
             //Send html page from wwwroot folder
-            //app.MapGet("/", () => Results.Content(File.ReadAllText($"{static_path}/index.html"), "text/html"));
+            app.MapGet("/", () => Results.Content(File.ReadAllText($"{static_path}/index.html"), "text/html"));
 
             //Send a JSON object
             app.MapGet("/api/test", () => new { hmm = "wow", bab = 12345 });
